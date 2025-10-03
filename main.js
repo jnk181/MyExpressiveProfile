dfbc802ed7748d10f5435c0b77b78e2bc76e2124
jnk181 <janko.cekovic@gmail.com>
2025-10-03 11:25:56 +0200

    commit

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ var photo_albums=[
         icon:"./img/icons/cat_cookie.png",
         photos:[
             {
-                src:"./media/photos/tufny.jpg",
+                src:"./media/photos/cats_tufny.jpg",
                 caption:"Tufnica",
                 date:"2025-03-20",
             },
             {
-                src:"./media/photos/micky.jpg",
+                src:"./media/photos/cats_micky.jpg",
                 caption:"Mikica",
                 date:"2024-12-13",
             }
@@ -50,6 +50,11 @@ var photo_albums=[
                 src:"./media/photos/nat5.jpg",
                 caption:"",
                 date:"2024-12-13",
+            },
+            {
+                src:"./media/photos/nat6.jpg",
+                caption:"",
+                date:"2024-12-13",
             }
         ]
     },
@@ -59,17 +64,22 @@ var photo_albums=[
         photos:[
             {
                 src:"./media/photos/italy1.jpg",
-                caption:"",
+                caption:"Tellaro",
                 date:"2024-12-13",
             },
             {
                 src:"./media/photos/italy2.jpg",
-                caption:"",
+                caption:"Siena",
                 date:"2024-12-13",
             },
             {
                 src:"./media/photos/italy3.jpg",
-                caption:"",
+                caption:"Malcesine",
+                date:"2024-12-13",
+            },
+            {
+                src:"./media/photos/italy4.jpg",
+                caption:"Lago di Braes",
                 date:"2024-12-13",
             }
         ]
@@ -97,6 +107,11 @@ var photo_albums=[
                 src:"./media/photos/old4.jpg",
                 caption:"Vojvodina",
                 date:"2019",
+            },
+            {
+                src:"./media/photos/old5.jpg",
+                caption:"Pink sunrise",
+                date:"2019",
             }
         ]
     },
@@ -104,11 +119,35 @@ var photo_albums=[
 
 var notes=[
     {
-        title:"Title",
+        title:"Beachball",
         content:"<h1>Test</h1>",
+        thumbnail:"./img/genericimage.jpg",
+        short_description: "I like beachballs",
+        date:"2025-10-02",
+    },
+    {
+        title:"Coffee",
+        content:"<h1>Coffee is good sometimes</h1>",
+        thumbnail:"",
+        short_description: "I like coffee",
         date:"2025-10-02",
     }
 ]
+
+function previewNote(ind) {
+        document.querySelector(`.overlay-black`).style.display="";
+        document.querySelector(`.overlay-black .note-preview`).style.display="";
+        document.querySelector(`.overlay-black .note-preview .header span.title`).innerHTML=notes[ind].title;
+        document.querySelector(`.overlay-black .note-preview .inner-content h1.title`).innerHTML=notes[ind].title;
+        document.querySelector(`.overlay-black .note-preview .inner-content .markdown-content`).innerHTML=notes[ind].content;
+        document.querySelector(`.overlay-black .note-preview .inner-content span.date`).innerHTML=notes[ind].date;
+    }
+
+function previewImage(image) {
+    document.querySelector(`.overlay-black`).style.display="";
+    document.querySelector(`.overlay-black .image-preview`).style.display="";
+    document.querySelector(`.overlay-black .image-preview .inner-content img.main`).src=image;
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector(`span.profilename`).innerHTML=config.name;
@@ -137,6 +176,9 @@ document.addEventListener('DOMContentLoaded', function() {
             photo_node.querySelector(`img.photo-main`).src=photo.src;
             photo_node.querySelector(`span.caption`).innerHTML=photo.caption;
             photo_node.classList.remove('template');
+            photo_node.addEventListener('click', function() {
+                previewImage(photo.src)
+            })
             new_node.querySelector(`.album-content`).appendChild(photo_node)
         })
         new_node.classList.remove('template');
@@ -150,10 +192,30 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     })
 
+    var cnt=0;
+    notes.forEach(note => {
+        var new_node = document.querySelector(`.note-item.template`).cloneNode(true);
+        new_node.classList.remove("template");
+        new_node.querySelector(`h1.title`).innerHTML=note.title;
+        new_node.querySelector(`.date-header span`).innerHTML=note.date;
+        new_node.dataset.index=cnt;
+        cnt++;
+
+        if(note.thumbnail!="") {
+            new_node.querySelector(`img.thumb`).style.display="";
+            new_node.querySelector(`img.thumb`).src=note.thumbnail;
+        }
+        else if(note.short_description!="") {
+            new_node.querySelector(`p.short-description`).style.display="";
+            new_node.querySelector(`p.short-description`).innerHTML=note.short_description;
+        }
+
+        document.querySelector(`.profile-notes .inner-content`).appendChild(new_node);
+    });
+
     document.querySelectorAll(`.note-item`).forEach(item => {
         item.addEventListener('click', function() {
-            document.querySelector(`.overlay-black`).style.display="";
-            document.querySelector(`.overlay-black .note-preview`).style.display="";
+            previewNote(item.dataset.index)
         })
     })
 });
